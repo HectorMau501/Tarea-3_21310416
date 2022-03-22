add223ed78440d647859efe2f2f8d9fac377ced1
HectorMau501 <mauhector7@gmail.com>
2022-03-22 09:00:26 -0600

    Agregado de la clase "Civil"

--- Herencia.cpp
+++ Herencia.cpp
@@ -16,14 +16,34 @@ using namespace std;
 //Clase Padre "Unidad"
 class Unidad{
 
+    //Atributos
     private:
         string name;
         int life;
 
+    //Metodos
     public:
-        Unidad(string , int);
+        Unidad(string , int); //Constructor
         void mostrarUnidad();
 };
+
+
+//Clase Civil hija de Unidad
+class Civil : public Unidad{
+
+    private:
+        string build; 
+        string repair;
+
+    public:
+        Civil(string , int , string , string);
+        void mostrarCivil();
+};
+
+
+
+
+
 
 
 
@@ -36,6 +56,12 @@ Unidad::Unidad(string _name , int _life){
     life = _life;
 }
 
+
+//Constructor Civil Hija de Unidad 
+Civil::Civil(string _name , int _life , string _build , string _repair) : Unidad(_name , _life){
+    build = _build;
+    repair = _repair;
+}
 
 
 
@@ -50,7 +76,12 @@ void Unidad::mostrarUnidad(){
 }
 
 
-
+//Metodo mostrar de Civil
+void Civil::mostrarCivil(){
+    mostrarUnidad();
+    cout<<"El puede construir: "<<build<<endl;
+    cout<<"EL puede reparar: "<<repair<<endl;
+}
 
 
 
@@ -63,9 +94,13 @@ int main(){
 	cout<<"\t\tHector Mauricio Rodriguez Salazar 21310314"<<endl;
 	cout<<"\n\n\t\tPrograma con Herencia"<<endl;
 
+    cout<<"--Unidad--"<<endl;
     Unidad unidad1("Caballero" , 80);
-
     unidad1.mostrarUnidad();
+
+    cout<<"\n\n--Civil--"<<endl;
+    Civil civil1("Aldeano" , 50 , "Edificios , Casas , Cuarteles." , "Unidades de asedio , Edificios , Casas , Cuarteles.");
+    civil1.mostrarCivil();
 
     system("pause");
     return 0;
