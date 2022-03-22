e3141c59380f58ddba0b20bd55f94fff6a5734dc
HectorMau501 <mauhector7@gmail.com>
2022-03-22 08:24:26 -0600

    Inicio de la tarea III "Herencia"

---
+++ Herencia.cpp
@@ -0,0 +1,41 @@
+/*Ceti Colomos
+Docente: Luis Manuel Morales Medina
+Hector Mauricio Rodriguez Salazar 21310416
+Grupo y Grado 2P2*/
+
+
+//Bibliotecas
+#include <stdlib.h>
+#include <iostream>
+
+using namespace std;
+
+
+
+
+
+//Funcion Principal
+int main(){
+
+    cout<<"\t\tBienvenido"<<endl;
+	cout<<"\t\tHector Mauricio Rodriguez Salazar 21310314"<<endl;
+	cout<<"\n\n\t\tPrograma con Herencia"<<endl;
+
+
+
+    system("pause");
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
