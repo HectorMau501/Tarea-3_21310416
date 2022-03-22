2f836a917dee3c6431af9ca418d65e39e18d3ae1
HectorMau501 <mauhector7@gmail.com>
2022-03-22 08:40:56 -0600

    Agregado de la clase "Unidad" (Padre)

--- Herencia.cpp
+++ Herencia.cpp
@@ -11,17 +11,61 @@ Grupo y Grado 2P2*/
 using namespace std;
 
 
+//      ----Clases----
+
+//Clase Padre "Unidad"
+class Unidad{
+
+    private:
+        string name;
+        int life;
+
+    public:
+        Unidad(string , int);
+        void mostrarUnidad();
+};
+
+
+
+//      ----Constructores----
+
+
+//Constructor Padre "Unidad"
+Unidad::Unidad(string _name , int _life){
+    name = _name;
+    life = _life;
+}
+
+
+
+
+
+//      ----Metodos----
+
+
+//Metodo mostrar de Unidad
+void Unidad::mostrarUnidad(){
+    cout<<"El nombre de la unidad es: "<<name<<endl;
+    cout<<"La vida de la unidad es de: "<<life<<endl;
+}
+
+
+
+
+
 
 
 
 //Funcion Principal
 int main(){
 
-    cout<<"\t\tBienvenido"<<endl;
+    cout<<"\n\n\t\tBienvenido"<<endl;
 	cout<<"\t\tHector Mauricio Rodriguez Salazar 21310314"<<endl;
 	cout<<"\n\n\t\tPrograma con Herencia"<<endl;
 
+    Unidad unidad1("Caballero" , 80);
 
+    unidad1.mostrarUnidad();
 
     system("pause");
     return 0;
